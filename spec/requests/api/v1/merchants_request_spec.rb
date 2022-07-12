3ea5337846d0296f36d09ad9c483b29c73dffa37
Clay Ash <claycourtneyash@gmail.com>
2022-07-12 13:51:35 -0600

    Add test for merchant item api call, set up items api request spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -24,16 +24,24 @@ RSpec.describe "Merchants API" do
   it 'can return one merchant' do 
     id = create(:merchant).id 
 
-    get '/api/v1/merchants'
+    get "/api/v1/merchants/#{id}"
 
     merchant = JSON.parse(response.body, symbolize_names: true)
+
     expect(response).to be_successful
     expect(merchant).to be_a(Hash)
-    merchant[:data].each do |merch|
-    expect(merch).to include(:id)
-    expect(merch[:attributes]).to include(:name)
-    expect(merch[:attributes][:name]).to be_a(String)
-    expect(merch[:id]).to be_a(String)
-    end 
+
+    merch = merchant[:data]
+    expect( merch).to include(:id)
+    expect( merch[:attributes]).to include(:name)
+    expect( merch[:attributes][:name]).to be_a(String)
+    expect( merch[:id]).to be_a(String)
+  end
+
+  xit 'can sad path' do 
+    id = 0
+    get "/api/v1/merchants/#{id}"
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to be(404)
   end
 end
