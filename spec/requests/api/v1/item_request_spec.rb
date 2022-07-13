352744b4548ee7b6f71f209e8f9aed9125146da4
Clay Ash <claycourtneyash@gmail.com>
2022-07-12 19:03:37 -0600

    Add tests for item update and destroy, begin merchant data for an item test

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -40,4 +40,58 @@ RSpec.describe "Item api" do
     expect(item[:attributes][:unit_price]).to be_a(Float)
     expect(item[:attributes][:merchant_id]).to eq(merchant.id)
   end
+
+  it 'can create an item' do 
+    merchant = create(:merchant)
+    item_params = { name: "The Thing", description: "It does the stuff", unit_price: 5.99 , merchant_id: merchant.id }
+    headers = {'CONTENT_TYPE' => 'application/json'}
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+
+    item = Item.last 
+
+    expect(response).to be_successful 
+    expect(response.status).to eq(201)
+    expect(item.name).to eq(item_params[:name])
+    expect(item.description).to eq(item_params[:description])
+    expect(item.unit_price).to eq(item_params[:unit_price])
+    expect(item.merchant_id).to eq(item_params[:merchant_id])
+  end
+
+  it 'can update an item' do 
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+    updated_item_params = { name: "The Thing", description: "It does the stuff", unit_price: 5.99 , merchant_id: merchant.id }
+    headers = {'CONTENT_TYPE' => 'application/json'}
+
+    patch "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate(item: updated_item_params)
+
+    changed_item = Item.last 
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(changed_item.name)
+    expect(item.description).to_not eq(changed_item.description)
+    expect(item.unit_price).to_not eq(changed_item.unit_price)
+  end
+
+  it 'can delete an item' do 
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful 
+    expect(Item.count).to eq(0)
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  xit 'can get merchant data for an item' do 
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+
+    get "/api/v1/items/#{item.id}/merchant"
+
+  end
 end
